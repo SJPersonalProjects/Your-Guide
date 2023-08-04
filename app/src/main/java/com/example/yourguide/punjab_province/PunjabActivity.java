81001eb9dbd1f38195568179ac88acfc4bf950a9
Sarim Jokhio <sarimjokhioprivate@gmail.com>
2023-08-04 16:59:29 +0500

    Added second horizontal list of cards for top restaurants in the activity and updated the necessary files

--- app/src/main/java/com/example/yourguide/punjab_province/PunjabActivity.java
+++ app/src/main/java/com/example/yourguide/punjab_province/PunjabActivity.java
@@ -21,34 +21,62 @@ public class PunjabActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_punjab);
 
-        ArrayList<PunjabProvinceCard> punjabProvinceCardsList = new ArrayList<>();
-        punjabProvinceCardsList.add(new PunjabProvinceCard(R.drawable.lahore_city, R.string.lahore_city_title,
+        //ArrayList to hold the top popular cities in Punjab Province...
+        ArrayList<PunjabProvinceCard> popularCitiesList = new ArrayList<>();
+        popularCitiesList.add(new PunjabProvinceCard(R.drawable.lahore_city, R.string.lahore_city_title,
                 R.string.lahore_city_description, R.string.lahore_city_rating, R.string.lahore_city_review));
-        punjabProvinceCardsList.add(new PunjabProvinceCard(R.drawable.faisalabad_city, R.string.faisalabad_city_title,
+        popularCitiesList.add(new PunjabProvinceCard(R.drawable.faisalabad_city, R.string.faisalabad_city_title,
                 R.string.faisalabad_city_description, R.string.faisalabad_city_rating, R.string.faisalabad_city_review));
-        punjabProvinceCardsList.add(new PunjabProvinceCard(R.drawable.rawalpindi_city, R.string.rawalpindi_city_title,
+        popularCitiesList.add(new PunjabProvinceCard(R.drawable.rawalpindi_city, R.string.rawalpindi_city_title,
                 R.string.rawalpindi_city_description, R.string.rawalpindi_city_rating, R.string.rawalpindi_city_review));
-        punjabProvinceCardsList.add(new PunjabProvinceCard(R.drawable.multan_city, R.string.multan_city_title,
+        popularCitiesList.add(new PunjabProvinceCard(R.drawable.multan_city, R.string.multan_city_title,
                 R.string.multan_city_description, R.string.multan_city_rating, R.string.multan_city_review));
-        punjabProvinceCardsList.add(new PunjabProvinceCard(R.drawable.gujranwala_city, R.string.gujranwala_city_title,
+        popularCitiesList.add(new PunjabProvinceCard(R.drawable.gujranwala_city, R.string.gujranwala_city_title,
                 R.string.gujranwala_city_description, R.string.gujranwala_city_rating, R.string.gujranwala_city_review));
 
         RecyclerView punjabProvinceRecyclerView = (RecyclerView) findViewById(R.id.punjab_province_recycler_view);
         punjabProvinceRecyclerView.setVerticalScrollBarEnabled(false);
         punjabProvinceRecyclerView.setHorizontalScrollBarEnabled(false);
         punjabProvinceRecyclerView.setHasFixedSize(true);
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        PunjabProvinceCardAdapter adapter = new PunjabProvinceCardAdapter(punjabProvinceCardsList);
+        RecyclerView.LayoutManager citiesLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        PunjabProvinceCardAdapter adapter = new PunjabProvinceCardAdapter(popularCitiesList);
 
-        punjabProvinceRecyclerView.setLayoutManager(mLayoutManager);
+        punjabProvinceRecyclerView.setLayoutManager(citiesLayoutManager);
         punjabProvinceRecyclerView.setAdapter(adapter);
 
         adapter.setOnItemClickListener(new PunjabProvinceCardAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(int position) {
                 //Toast to show the click event message of cards in Punjab Activity.
-                Toast.makeText(PunjabActivity.this, punjabProvinceCardsList.get(position).getCardTitle(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(PunjabActivity.this, popularCitiesList.get(position).getCardTitle(), Toast.LENGTH_SHORT).show();
             }
         });
+
+        //ArrayList to hold the top restaurants in Punjab Province.
+        ArrayList<PunjabProvinceCard> topRestaurantList = new ArrayList<>();
+        topRestaurantList.add(new PunjabProvinceCard(R.drawable.monal_restaurant, R.string.monal_restaurant_title,
+                R.string.monal_restaurant_description, R.string.monal_restaurant_rating, R.string.monal_restaurant_review));
+        topRestaurantList.add(new PunjabProvinceCard(R.drawable.haveli_restaurant, R.string.haveli_restaurant_title,
+                R.string.haveli_restaurant_description, R.string.haveli_restaurant_rating, R.string.haveli_restaurant_review));
+        topRestaurantList.add(new PunjabProvinceCard(R.drawable.andaaz_restaurant, R.string.andaaz_restaurant_title,
+                R.string.andaaz_restaurant_description, R.string.andaaz_restaurant_rating, R.string.andaaz_restaurant_review));
+        topRestaurantList.add(new PunjabProvinceCard(R.drawable.spice_bazaar, R.string.spice_bazar_title, R.string.spice_bazar_description,
+                R.string.spice_bazar_rating, R.string.spice_bazar_review));
+        topRestaurantList.add(new PunjabProvinceCard(R.drawable.dera_restaurant, R.string.dera_restaurant_title,
+                R.string.dera_restaurant_description, R.string.dera_restaurant_rating, R.string.dera_restaurant_review));
+
+        RecyclerView restaurantsRecyclerView = (RecyclerView) findViewById(R.id.top_restaurant_recycler_view);
+        restaurantsRecyclerView.setVerticalScrollBarEnabled(false);
+        restaurantsRecyclerView.setHorizontalScrollBarEnabled(false);
+        restaurantsRecyclerView.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager restaurantsLayoutManager = new LinearLayoutManager(this,
+                LinearLayoutManager.HORIZONTAL, false);
+
+        PunjabProvinceCardAdapter restaurantsAdapter = new PunjabProvinceCardAdapter(topRestaurantList);
+
+        restaurantsRecyclerView.setLayoutManager(restaurantsLayoutManager);
+        restaurantsRecyclerView.setAdapter(restaurantsAdapter);
+
     }
 }
