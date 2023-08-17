2852d581867e143a968a172ff825599eef00c4f3
Sarim Jokhio <sarimjokhioprivate@gmail.com>
2023-08-17 12:36:48 +0500

    Added the layout file for restaurants in the cards category

---
+++ app/src/main/java/com/example/yourguide/province_cards_categories/TopRestaurantActivity.java
@@ -0,0 +1,16 @@
+package com.example.yourguide.province_cards_categories;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.yourguide.R;
+
+public class TopRestaurantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_restaurant);
+    }
+}
